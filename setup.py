564eeb3a63f292c5a38c28e43e97d6d597159e69
abelsonlive <brianabelson@gmail.com>
2014-07-01 02:08:55 -0400

    vbump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except (IOError, ImportError):
 # setup
 setup(
   name='birdfeeder',
-  version='0.0.9',
+  version='0.1.0',
   description='A newslynx-opinionated wrapper around twython',
   long_description = long_description,
   classifiers=[
