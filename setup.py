d1eb54252d60df47854a66b5b41b76fd9a7864c5
abelsonlive <brianabelson@gmail.com>
2014-09-28 13:40:57 -0400

    fuck

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except (IOError, ImportError):
 # setup
 setup(
   name='birdfeeder',
-  version='0.1.7',
+  version='0.1.8',
   description='A newslynx-opinionated wrapper around twython',
   long_description = long_description,
   classifiers=[
